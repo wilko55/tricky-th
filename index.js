e97fc34cf5e82b8232057e6f1047e0fd0e8efba6
Andrew Wilkinson <andyjwilko@gmail.com>
2016-05-25 00:06:08 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require('express')
+var app = express()
+var path = require('path');
+
+// view engine setup
+app.set('view engine', 'html');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(require('express-partial-templates')(app));
+app.engine('html', require('hogan-express-strict'));
+ 
+console.log('Up on port 5000')
+app.listen(5000)
